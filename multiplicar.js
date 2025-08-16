b213d4e141aa2ff800359fecac8b2c03d4f86d31
Victor gabriel almeida prado <victorgapsilva1@senacsp.edu.br>
2025-08-15 23:46:19 -0300

    terminado calculadora

---
+++ multiplicar.js
@@ -0,0 +1,6 @@
+function multiplicar(){
+    const a = parseFloat(document.getElementById("numero1").value);  0;
+    const b = parseFloat(document.getElementById("numero2").value);  0;
+    const resultado = a * b;
+    document.getElementById("resultado").value = resultado;
+}
